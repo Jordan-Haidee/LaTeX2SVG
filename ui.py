5421237bc60a1e3f7d49ac46580b8aef3b27f7ed
Jordan <2807427568@qq.com>
2023-12-23 21:17:57 +0800

    use a nice theme

--- ui.py
+++ ui.py
@@ -24,14 +24,15 @@ def get_svg_image(latex_cmd: str, is_algo: bool = False) -> str:
 
 
 if __name__ == "__main__":
-    with gradio.Blocks() as demo:
+    theme = gradio.themes.Soft()
+    with gradio.Blocks(theme) as demo:
         with gradio.Row():
-            output_image_box = gradio.Image(width=500, height=500, type="filepath")
+            output_image_box = gradio.Image(width=500, height=500, type="filepath", label="Output SVG")
             with gradio.Column():
                 latex_input_box = gradio.Textbox(
                     lines=16, label="LaTeX", placeholder="Please input LaTeX equation code here"
                 )
                 is_algo_checkbox = gradio.Checkbox(label="Pseudocode")
-                submit_btn = gradio.Button("submit")
+                submit_btn = gradio.Button("Submit")
             submit_btn.click(fn=get_svg_image, inputs=latex_input_box, outputs=output_image_box)
     demo.launch(server_name="0.0.0.0", server_port=8001)
