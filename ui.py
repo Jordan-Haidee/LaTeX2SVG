5f073892f4c9b3b9a87a2953e368028084adc062
Jordan <2807427568@qq.com>
2023-12-25 19:56:29 +0800

    beautify WebUI

--- ui.py
+++ ui.py
@@ -6,15 +6,14 @@ import gradio
 import httpx
 
 
-def get_svg_image(latex_cmd: str, is_algo: bool = False) -> str:
+def pipeline(latex_cmd: str, algo_name: str = None) -> str:
     url = "http://127.0.0.1:8000/latex2svg/"
-    response = httpx.get(
-        url,
-        params={
-            "latex_cmd": latex_cmd,
-            "task_type": "simple" if not is_algo else "simple_with_algo",
-        },
-    )
+    data = {
+        "latex_cmd": latex_cmd,
+        "type": "simple" if not bool(algo_name) else "simple_with_algo",
+        "algo_name": algo_name,
+    }
+    response = httpx.post(url, json=data)
     svg_code = response.text
     temp_directory = Path(tempfile.gettempdir())
     temp_svg_path = (temp_directory / str(uuid.uuid4())).with_suffix(".svg")
@@ -29,9 +28,13 @@ if __name__ == "__main__":
             output_image_box = gradio.Image(width=500, height=500, type="filepath", label="Output SVG")
             with gradio.Column():
                 latex_input_box = gradio.Textbox(
-                    lines=16, max_lines=17, label="LaTeX", placeholder="Please input LaTeX equation code here"
+                    lines=13, max_lines=13, label="LaTeX", placeholder="Please input LaTeX code here ..."
                 )
-                is_algo_checkbox = gradio.Checkbox(label="Pseudocode")
+                with gradio.Row():
+                    algo_name_box = gradio.Textbox(
+                        show_label=False,
+                        placeholder="Algorithm name of pseudocode ...\nblank means it's a normal LaTeX math equation",
+                    )
                 submit_btn = gradio.Button("Submit")
-            submit_btn.click(fn=get_svg_image, inputs=[latex_input_box, is_algo_checkbox], outputs=output_image_box)
+            submit_btn.click(fn=pipeline, inputs=[latex_input_box, algo_name_box], outputs=output_image_box)
     demo.launch(server_name="0.0.0.0", server_port=8001)
