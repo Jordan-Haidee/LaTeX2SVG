88f6451fb7020ad575ff6982536e72f986db4c93
Jordan <2807427568@qq.com>
2023-12-26 23:27:17 +0800

    change to use  TEMP directory rather than `./result`

--- serve.py
+++ serve.py
@@ -1,11 +1,12 @@
 import subprocess
-from datetime import datetime
+import tempfile
+import uuid
 from pathlib import Path
 from typing import Literal
 
-import latex2mathml.converter
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
+from latex2mathml import converter
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -60,33 +61,32 @@ def latex2pdf(input_file: str, output_file: str) -> bool:
 @app.post("/latex2svg/")
 def latex2svg(task_data: TaskData) -> str:
     # create save directory
-    current_time = datetime.now()
-    current_timestamp = int(current_time.timestamp())
-    save_dir = Path("result") / current_time.strftime(r"%Y-%m-%d-%H-%M-%S")
-    save_dir.mkdir(exist_ok=True, parents=True)
+    task_id = str(uuid.uuid4())
+    task_dir = Path(tempfile.gettempdir()) / task_id
+    task_dir.mkdir()
     # convert pipeline
-    template_path = Path(__file__).parent / "templates" / f"{task_data.type}.tex"
+    template_path = Path(__file__).parent / "templates/{}.tex".format(task_data.type)
     with open(template_path, "r") as f:
         latex_template = f.read()
     if task_data.type == "simple":
         latex_cmd = latex_template % task_data.latex_cmd
     else:
         latex_cmd = latex_template % (task_data.algo_name, task_data.latex_cmd)
-    tex_path = save_dir / f"{current_timestamp}.tex"
+    tex_path = task_dir / "{}.tex".format(task_id)
     pdf_path = tex_path.with_suffix(".pdf")
     with open(tex_path, "w") as f:
         f.write(latex_cmd)
     res = latex2pdf(tex_path.absolute(), tex_path.parent.absolute())
     if res is False:
-        raise HTTPException(301, detail="when converting to pdf, error happened !")
+        raise HTTPException(351, detail="when converting to pdf, error happened !")
     pdf_cropped_path = pdf_path.parent / (pdf_path.stem + "_cropped.pdf")
     res = crop_pdf(pdf_path, pdf_cropped_path)
     if res is False:
-        raise HTTPException(302, detail="when cropping pdf, error happened !")
+        raise HTTPException(352, detail="when cropping pdf, error happened !")
     svg_path = pdf_path.with_suffix(".svg")
     res = pdf2svg(pdf_cropped_path, svg_path)
     if res is False:
-        raise HTTPException(303, detail="when converting to svg, error happened !")
+        raise HTTPException(353, detail="when converting to svg, error happened !")
     # return response
     response = FileResponse(svg_path)
     if task_data.download is True:
@@ -96,12 +96,11 @@ def latex2svg(task_data: TaskData) -> str:
 
 @app.post("/latex2mathml/")
 def latex_to_mathml(task_data: TaskData):
-    current_time = datetime.now()
-    current_timestamp = int(current_time.timestamp())
-    save_dir = Path("result") / current_time.strftime(r"%Y-%m-%d-%H-%M-%S")
-    save_dir.mkdir(exist_ok=True, parents=True)
-    output_file = save_dir / f"{current_timestamp}.xml"
-    mathml_code = latex2mathml.converter.convert(task_data.latex_cmd.strip())
+    task_id = str(uuid.uuid4())
+    task_dir = Path(tempfile.gettempdir()) / task_id
+    task_dir.mkdir()
+    output_file = task_dir / "{}.xml".format(task_id)
+    mathml_code = converter.convert(task_data.latex_cmd.strip())
     with open(output_file, "w") as f:
         f.write(mathml_code)
     response = FileResponse(output_file)
